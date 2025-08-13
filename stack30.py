7ad4fe453e6eff7f3e538304cf78f8ee9ea3c8a7
Kartik  K Bandekar <bandekar.kartik117@gmail.com>
2025-08-13 09:53:37 +0530

    Add files via upload

---
+++ stack30.py
@@ -0,0 +1,40 @@
+class Stack:
+    def __init__(self):
+        self.stack= []
+
+    def push (self,item):
+        self.stack.append(item)
+        print(f"pushed{item}")
+
+    def pop(self):
+        if self.is_empty():
+            return"stack is empty!"
+        return(f"popped:{self.stack.pop()}")
+    
+    def peek(self):
+        if self.is_empty():
+            return "stack is empty!"
+        return f"top:{self.stack[-1]}"
+    
+    def is_empty(self):
+        return len(self.stack)==0
+    
+    def size(self):
+        return len(self.Stack)
+    
+    def display (self):
+        return("Stack(top -> bottom):",self.stack[::-1])
+
+s = Stack()
+s.push(20)
+print(s.peek())
+s.push(30)
+s.display()
+print(s.pop())
+print(s.peek())
+s.display()
+
+
+
+
+    
